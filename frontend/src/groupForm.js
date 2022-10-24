12aed5feef389fe440c6127db26e9f6e96967b52
Ethan <ethanothepro@gmail.com>
2022-10-24 11:39:13 -0700

    test

---
+++ frontend/src/groupForm.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function groupForm() { 
+    return (
+      /*
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Job</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Charlie</td>
+            <td>Janitor</td>
+          </tr>
+          <tr>
+            <td>Mac</td>
+            <td>Bouncer</td>
+          </tr>
+          <tr>
+            <td>Dee</td>
+            <td>Aspiring actress</td>
+          </tr>
+          <tr>
+            <td>Dennis</td>
+            <td>Bartender</td>
+          </tr>
+        </tbody>
+      </table>
+      */
+     <p>hellop</p>
+    );
+}
+
+export default groupForm;
---
+++ frontend/src/profile.js
@@ -0,0 +1,21 @@
+import React, {useState, useEffect} from 'react';
+import groupForm from './groupForm';
+
+
+
+
+
+function Profile() {
+    return ( 
+        
+       <div className="container">
+            hello
+            <groupForm />
+
+       </div>
+
+
+    ); 
+}
+
+export default Profile;
