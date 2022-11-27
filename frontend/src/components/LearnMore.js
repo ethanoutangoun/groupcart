f806b9b31abb72d1b1f6c7d4feb61ee5d9a614f9
kyuusk <ziadinnavarro4@gmail.com>
2022-11-27 03:17:46 -0800

    More changes to Learn More page

--- frontend/src/components/LearnMore.js
+++ frontend/src/components/LearnMore.js
@@ -10,21 +10,20 @@ function LearnMore() {
       <main className="purpose-content">
         <div className="purpose-header">
           <h1>Our Purpose</h1>
-          <p className="purpose-text">GroupCart provides a seamless platform for coordinating payment and aquisition of shared goods between roomates, families and organizations.</p>
+          <p className="purpose-text">GroupCart provides a seamless platform for coordinating payment and aquisition of shared goods between roomates, families, and organizations.</p>
           <p className="purpose-text">Easily keep track of orders and connect with others.</p>
-          <p className="purpose-text">Interested? Join GroupCart today</p>
-          <div>
+          <p className="purpose-text">Interested? Join GroupCart today!</p>
+          <div className="purpose-footer">
             <NavLink className="purpose-link hover-underline-animation" to="/sign-in">
               <p className="purpose-text join-link">Login ↗</p>
             </NavLink>
-
             <NavLink className="purpose-link hover-underline-animation" to="/sign-up">
               <p className="purpose-text join-link">Register ↗</p>
             </NavLink>
+            <p className="purpose-footer-text">
+              🛒💸
+            </p>
           </div>
-          <p className="purpose-footer">
-            🛒💸
-          </p>
         </div>
         
       </main>
