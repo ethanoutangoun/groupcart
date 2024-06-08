ad447ba0d1f72cf7414a555b72d9a120cbbaf6f6
masatonandate <62863678+masatonandate@users.noreply.github.com>
2024-06-07 18:18:59 -0700

    Deleted Commented Code

--- backend/routes/userroute.js
+++ backend/routes/userroute.js
@@ -4,17 +4,6 @@ import mongoose from "mongoose";
 import controller from "../controllers/user.controller.js";
 const router = express.Router();
 
-// router.post("/user", async (req, res) => {
-//   const item = req.body;
-//   const saveduser = await userservice.createUser(item);
-//   console.log(saveduser);
-//   if (saveduser) {
-//     res.status(201).send(saveduser);
-//   } else {
-//     res.status(500).end();
-//   }
-// });
-
 //login route
 router.post("/login", controller.loginUser);
 
